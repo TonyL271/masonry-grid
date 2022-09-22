5e636bbec7574262280b9b4b7300392b8dce42d3
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-09-22 14:19:36 -0700

    Masonry grid layout now works

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var grid = document.querySelector('.grid');
+
+    var msnry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+    });
+
+    imagesLoaded(grid).on('progress', function () {
+        // layout Masonry after each image loads
+        msnry.layout();
+    });
+}, false);
